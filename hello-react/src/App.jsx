8ccb643950098ee81f0b390e8f6efb87655b1826
sanket <sanketjambhulkar018@gmail.com>
2023-08-01 22:46:12 +0530

    correct typos

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -7,12 +7,12 @@ function App() {
   <div>
     <h1>{message}</h1>
     <div>
-      <h1>pending: </h1>
+      <h1>pending</h1>
       <TaskCard title="update blog" dueDate="20/20/2020" assigneeName="sanket jambhulkar"/>
       <TaskCard title="Email client" dueDate="21/20/2020" assigneeName="sanket jambhulkar"/>
     </div>
     <div>
-      <h1>completed: </h1>
+      <h1>done</h1>
       <TaskCard title="washing clothes " CompletedAtDate="18/20/2020" assigneeName="sanket jambhulkar" />
       <TaskCard title="check Emails" CompletedAtDate="09/20/2020" assigneeName="sanket jambhulkar" />
     </div>
