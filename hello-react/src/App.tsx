f16307e9dea693f4bcd69efdce79d902e178aae4
sanket <sanketjambhulkar018@gmail.com>
2023-08-02 10:04:11 +0530

    changed project to typescript

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -1,4 +1,4 @@
-import TaskCard from './TaskCard.jsx'
+import TaskCard from './TaskCard.tsx'
 
 function App() {
   const message = "todo-manager app"
@@ -8,13 +8,14 @@ function App() {
     <h1>{message}</h1>
     <div>
       <h1>pending</h1>
-      <TaskCard title="update blog" dueDate="20/20/2020" assigneeName="sanket jambhulkar"/>
-      <TaskCard title="Email client" dueDate="21/20/2020" assigneeName="sanket jambhulkar"/>
+      <TaskCard title="update blog" dueDate="27/20/2020" assigneeName="sanket jambhulkar"/>
+      <TaskCard title="Email client!" dueDate="21/20/2020" assigneeName="sanket jambhulkar"/>
     </div>
     <div>
       <h1>done</h1>
       <TaskCard title="washing clothes " CompletedAtDate="18/20/2020" assigneeName="sanket jambhulkar" />
       <TaskCard title="check Emails" CompletedAtDate="09/20/2020" assigneeName="sanket jambhulkar" />
+      <TaskCard title="eat roti! " CompletedAtDate="09/20/2020" assigneeName="sanket jambhulkar" />
     </div>
   </div>
   )
