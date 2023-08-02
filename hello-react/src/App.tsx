9b68edcc71185f6652706dbc78246f3050359aae
sanket <sanketjambhulkar018@gmail.com>
2023-08-02 10:20:41 +0530

    modify code

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -1,24 +1,24 @@
-import TaskCard from './TaskCard.tsx'
+import TaskCard from './TaskCard.tsx';
 
 function App() {
-  const message = "todo-manager app"
+  const message = "todo-manager app";
 
   return (
-  <div>
-    <h1>{message}</h1>
     <div>
-      <h1>pending</h1>
-      <TaskCard title="update blog" dueDate="27/20/2020" assigneeName="sanket jambhulkar"/>
-      <TaskCard title="Email client!" dueDate="21/20/2020" assigneeName="sanket jambhulkar"/>
+      <h1>{message}</h1>
+      <div>
+        <h1>pending</h1>
+        <TaskCard title="update blog" dueDate="27/20/2020" assigneeName="sanket jambhulkar" />
+        <TaskCard title="Email client!" dueDate="21/20/2020" assigneeName="sanket jambhulkar" />
+      </div>
+      <div>
+        <h1>done</h1>
+        <TaskCard title="washing clothes" completedAtDate="18/20/2020" assigneeName="sanket jambhulkar" />
+        <TaskCard title="check Emails" completedAtDate="09/20/2020" assigneeName="sanket jambhulkar" />
+        <TaskCard title="eat roti!" completedAtDate="09/20/2020" assigneeName="sanket jambhulkar" />
+      </div>
     </div>
-    <div>
-      <h1>done</h1>
-      <TaskCard title="washing clothes " CompletedAtDate="18/20/2020" assigneeName="sanket jambhulkar" />
-      <TaskCard title="check Emails" CompletedAtDate="09/20/2020" assigneeName="sanket jambhulkar" />
-      <TaskCard title="eat roti! " CompletedAtDate="09/20/2020" assigneeName="sanket jambhulkar" />
-    </div>
-  </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,24 +1,24 @@
-import './TaskCard.css'
+import './TaskCard.css';
 
 const TaskCard = (props) => {
-  if(props.dueDate){
-  return (
-    <div className='TaskItem'>
-      <h2 className="text-xl font-bold">{props.title}</h2>
-      <p>due date: {props.dueDate}</p>
-      <p>Assignee: {props.assigneeName} </p>
-    </div>
-  )
-  }
-  if(props.CompletedAtDate){
+  if (props.dueDate) {
     return (
-    <div className='TaskItem'>
-      <h2 className="text-xl font-bold">{props.title}</h2>
-      <p>Completed on: {props.CompletedAtDate}</p>
-      <p>Assignee: {props.assigneeName}</p>
-    </div>
-    )
+      <div className='TaskItem'>
+        <h2 className="text-xl font-bold">{props.title}</h2>
+        <p>Due on: {props.dueDate}</p>
+        <p>Assignee: {props.assigneeName}</p>
+      </div>
+    );
   }
-}
+  if (props.completedAtDate) {
+    return (
+      <div className='TaskItem'>
+        <h2 className="text-xl font-bold">{props.title}</h2>
+        <p>Completed on: {props.completedAtDate}</p>
+        <p>Assignee: {props.assigneeName}</p>
+      </div>
+    );
+  }
+};
 
-export default TaskCard
+export default TaskCard;
