38facefbdc67420e9c629d0be469138118fa2ac7
sanket <sanketjambhulkar018@gmail.com>
2023-08-26 21:19:10 +0530

    spelling mistake

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -13,7 +13,7 @@ const NotfoundPage: React.FC = () => {
                             </h1>
                             <p className="my-2 text-gray-800">Sorry about that! Please visit our hompage to get where you need to go.</p>
                             <button
-            id="backtoHomeButton"
+            id="backToHomeButton"
             className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700"
           >
             <a href="/home">Back to home</a>
