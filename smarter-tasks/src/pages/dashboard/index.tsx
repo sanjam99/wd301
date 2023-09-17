4d8907c6ae2139261ea31248af7a7e70635706dc
sanket <sanketjambhulkar018@gmail.com>
2023-09-17 14:48:19 +0530

    M6

---
+++ smarter-tasks/src/pages/dashboard/index.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+const Dashboard: React.FC = () => {
+  const navigate = useNavigate()
+  const [user, setUser] = useState<{ name: string; email: string } | null>(null);
+  useEffect(() => {
+    const userData = localStorage.getItem('userData');
+    if (userData) {
+      try {
+        const parsedUserData = JSON.parse(userData);
+        setUser(parsedUserData);
+      } catch (error) {
+        console.error('Error parsing userData:', error);
+        setUser(null);
+      }
+    }
+  }, []);
+  const logout = () => { 
+     localStorage.removeItem('authToken'); 
+     localStorage.removeItem('userData'); 
+     navigate('/signin');
+   };
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="py-8">
+        <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Dashboard</h1>
+        <div className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md">
+          {user ? (
+            <div>
+              <p className="text-lg font-semibold mb-4">Name: {user.name}</p>
+              <p className="text-lg font-semibold">Email: {user.email}</p>      
+              <button 
+             id="backToHomeButton" 
+             className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-green-600 active:bg-green-600 hover:bg-green-700" 
+           > 
+             <a href="#" onClick= {logout}>logout</a>
+             </button>
+            </div>
+          ) : (
+            <p className="text-lg text-center text-gray-600">No user data available.</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
--- smarter-tasks/src/types.ts
+++
@@ -1,7 +0,0 @@
-export interface TaskItem {
-  id?: number;
-  title: string;
-  description: string;
-  duedate: string;
-  // onDelete?: () => void;
-}
