f9e026da28133b09098b500e5d3cfce883f0c557
sanjam99 <142136015+sanjam99@users.noreply.github.com>
2023-09-27 08:31:26 +0000

    added members

--- smarter-tasks/src/pages/members/MemberList.tsx
+++ smarter-tasks/src/pages/members/MemberList.tsx
@@ -1,13 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { API_ENDPOINT } from '../../config/constants';
 interface Member {
   id: number;
   email: string,
   name: string;
 }
+interface State {
+  members: Member[];
+  isLoading: boolean;
+}
+interface Action {
+  type: string;
+  payload?: any;
+}
+const reducer = (state: State, action: Action): State => {
+  if (action.type === "API_CALL_START") {
+    return {
+      ...state,
+      isLoading: true
+    }
+  }
+  if (action.type === "API_CALL_END") {
+    return {
+      ...state,
+      isLoading: false,
+      members: action.payload,
+    }
+  }
+  if (action.type === "API_CALL_ERROR") {
+    return {
+      ...state,
+      isLoading: false
+    }
+  }
+  return state;
+}
 const MemberList: React.FC = () => {
-  const [members, setmembers] = useState<Member[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [state, dispatch] = useReducer(reducer, {
+    members: [],
+    isLoading: false
+  });
+//  const [members, setmembers] = useState<Member[]>([]);
+//  const [isLoading, setIsLoading] = useState<boolean>(false);
   useEffect(() => {
     // Fetch the list of projects here
     fetchProjects();
@@ -16,30 +50,32 @@ const MemberList: React.FC = () => {
     const token = localStorage.getItem("authToken") ?? "";
     console.log("tokens:",token);
     try {
-      setIsLoading(true);
+      dispatch({ type: "API_CALL_START" });
       const response = await fetch(`${API_ENDPOINT}/users`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json', "Authorization": `Bearer ${token}` },
       });
       const data = await response.json();
-      setmembers(data);
-      setIsLoading(false);
+      dispatch({ type: "API_CALL_END", payload: data });
+      //setmembers(data);
+      //setIsLoading(false);
     } catch (error) {
       console.log('Error fetching projects:', error);
-      setIsLoading(false);
+      dispatch({ type: "API_CALL_ERROR" });
     }
   };
   return (
     <div>
-      <h2>Project List</h2>
-      {isLoading ? (
-        <div>Loading...</div>
+      {state.isLoading ? (
+        <div>Loading...</div> // You can replace this with a progress bar component
       ) : (
-        <ul>
-          {members.map(member => (
-            <li key={member.id}>{member.name}<br></br>{member.email}</li>
+        <div className="grid gap-4 grid-cols-4 mt-5">
+          {state.members.map(member => (
+            <div key={member.id} className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+              <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">Name: {member.name}<br></br>email: {member.email}</h5>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
---
+++ smarter-tasks/src/pages/members/NewMember.tsx
@@ -0,0 +1,162 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Dialog, Transition } from '@headlessui/react'
+import { API_ENDPOINT } from '../../config/constants';
+// Next we will import Fragment and useState component from 'react'
+
+import { Fragment, useState } from 'react'
+
+type Inputs = {
+    name: string,
+    email: string,
+    password: string
+  };
+
+const NewMember = () => {
+    
+const [name, setName] = useState('');
+const [email, setemail] = useState('');
+const [password, setpassword] = useState('');
+const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+
+
+const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    // Let's remove event.PreventDefault()
+    const { name,email,password } = data
+    const token = localStorage.getItem("authToken") ?? "";
+    try {
+      const response = await fetch(`${API_ENDPOINT}/users`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', "Authorization": `Bearer ${token}` },
+        body: JSON.stringify({ name,email,password }),
+      });
+      // If response is not OK, in that case I'll throw an error.
+
+      if (!response.ok) {
+        throw new Error('Failed to create project');
+      }
+      // Next, I'll extract the response body as JSON data
+
+      const data = await response.json();
+
+      // Let's print the data in console
+
+      console.log(data)
+      setIsOpen(false)
+    } catch (error) {
+      // And in catch block, I'll print the error in console.
+
+      console.error('Operation failed:', error);
+    }
+  };
+// Then we will use useState hook to handle local state for dialog component
+
+  let [isOpen, setIsOpen] = useState(false)
+
+// Then we add the openModal function. 
+// If you don't know, Modal and Dialog are almost same thing.
+
+  const openModal = () => {
+    setIsOpen(true)
+  }
+
+  
+
+// Then we add the closeModal function
+  const closeModal = () => {
+    setIsOpen(false)
+  }
+
+// In the return statement, we will use the code for modal 
+// that we've obtained from https://headlessui.com/react/dialog
+
+  return (
+    <>
+      <button
+    type="button"
+    onClick={openModal}
+    className="rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-95 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+  >
+    New Member
+  </button>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Create new member
+                  </Dialog.Title>
+                  <div className="mt-2">
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+  type="text"
+  placeholder='Enter member name...'
+  autoFocus
+  {...register('name', { required: true })}
+  className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+    errors.name ? 'border-red-500' : ''
+  }`}
+/>
+{errors.name && <span>This field is required</span>}
+<input
+  type="text"
+  placeholder='Enter member email...'
+  autoFocus
+  {...register('email', { required: true })}
+  className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+    errors.name ? 'border-red-500' : ''
+  }`}
+/>
+{errors.name && <span>This field is required</span>}
+<input
+  type="text"
+  placeholder='Enter member password...'
+  autoFocus
+  {...register('password', { required: true })}
+  className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+    errors.name ? 'border-red-500' : ''
+  }`}
+/>
+{errors.name && <span>This field is required</span>}
+<button type="submit" className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
+    Submit
+  </button>
+  <button type="submit" onClick={closeModal} className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
+    Cancel
+  </button>
+</form>
+    </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>    
+    </>
+  )
+}
+// ...
+    export default NewMember;
