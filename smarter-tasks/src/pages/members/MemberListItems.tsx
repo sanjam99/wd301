bc1e068fbd6acd3ec94f5be12fc09462cbfd0dd3
sanket <sanketjambhulkar018@gmail.com>
2023-09-27 22:56:56 +0530

    small mistake

--- smarter-tasks/src/pages/members/MemberListItems.tsx
+++ smarter-tasks/src/pages/members/MemberListItems.tsx
@@ -30,7 +30,8 @@
      <> 
        {members.map((member: any) => ( 
          <div key={member.id} className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"> 
-           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">{member.name}</h5> 
+           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">Name:{member.name}</h5> 
+           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">email: {member.email}</h5>
          </div> 
        ))}         
      </> 
