66ddc7f7db433dcb8b0fa6e946335e4216e72233
sanjam99 <142136015+sanjam99@users.noreply.github.com>
2023-10-04 08:04:14 +0000

    hi

--- smarter-tasks/src/pages/members/MemberListItems.tsx
+++ smarter-tasks/src/pages/members/MemberListItems.tsx
@@ -26,7 +26,6 @@ export default function MemberListItems() {
           key={member.id}
           className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 relative"
           id="member"
-          class="member"
         >
           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">
             Name: {member.name}
