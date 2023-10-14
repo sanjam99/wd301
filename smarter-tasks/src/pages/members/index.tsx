ebd643b6cf2ca4c93c0fc2e4362ce2aa09351688
sanket <sanketjambhulkar018@gmail.com>
2023-10-14 18:19:46 +0530

    hi2

--- smarter-tasks/src/pages/members/index.tsx
+++ smarter-tasks/src/pages/members/index.tsx
@@ -1,20 +1,15 @@
-import React, { Suspense } from "react";
-const MemberList = React.lazy(() => import("./MemberList"));
-import NewMember from "./NewMember";
-import ErrorBoundary from "../../components/ErrorBoundary";
-const Members = () => {
-  return (
-    <>
-      <div className="flex justify-between">
-        <h2 className="text-2xl font-medium tracking-tight">Members</h2>
-        <NewMember />
-      </div>
-      <ErrorBoundary>
-        <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
-          <MemberList />
-        </Suspense>
-      </ErrorBoundary>
-    </>
-  );
-};
-  export default Members;
+import MemberList from "./MemberList"; 
+ import NewMember from "./NewMember"; 
+  
+ const Members = () => { 
+   return ( 
+     <> 
+       <div className="flex justify-between"> 
+         <h2 className="text-2xl font-medium tracking-tight">Members</h2> 
+         <NewMember /> 
+       </div> 
+       <MemberList /> 
+     </> 
+   ); 
+ }; 
+ export default Members;
