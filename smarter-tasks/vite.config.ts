1b58086a9463289d6d3a11e60c8beef7d1be9bf2
sanjam99 <142136015+sanjam99@users.noreply.github.com>
2023-10-17 22:57:22 +0530

    Update vite.config.ts

--- smarter-tasks/vite.config.ts
+++ smarter-tasks/vite.config.ts
@@ -18,13 +18,28 @@ export default defineConfig({
         short_name: "Smarter Tasks",
         icons: [
           {
-            "src": "/san.ico",
+            "src": "/favicon.ico",
             "sizes": "64x64 32x32 24x24 16x16",
             "type": "image/x-icon"
           },
           {
-            "src": "/san.gif",
-            "type": "image/gif",
+            "src": "/favicon-16x16.png",
+            "type": "image/png",
+            "sizes": "16x16"
+          },
+          {
+            "src": "/favicon-32x32.png",
+            "type": "image/png",
+            "sizes": "32x32"
+          },
+          {
+            "src": "/pwa-192x192.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          },
+          {
+            "srcpwa-512x512.png",
+            "type": "image/png",
             "sizes": "512x512",
             "purpose": "any maskable" // Icon format that ensures that your PWA icon looks great on all Android devices
           }
